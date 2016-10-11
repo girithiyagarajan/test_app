a5bdadfeebeb6ade79148d12b257ecc11773605b
Giri Thiyagarajan <girit@rigbot.com>
2016-10-11 16:54:01 +0000

    added home page and about page

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,15 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+    root 'pages#home'
+    get 'home/about', to: 'pages#about'
+    
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+    resources :articles
 
   # Example resource route with options:
   #   resources :products do
